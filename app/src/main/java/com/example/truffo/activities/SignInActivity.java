fdb12c3cfe4fed5e83d8ee5bd700a47004f1c34b
William Nevada <nguyenvanan12st@gmail.com>
2022-12-05 14:02:25 +0700

    Add firebase

--- app/src/main/java/com/example/truffo/activities/SignInActivity.java
+++ app/src/main/java/com/example/truffo/activities/SignInActivity.java
@@ -12,7 +12,6 @@ public class SignInActivity extends AppCompatActivity {
 
     private ActivitySignInBinding binding;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
