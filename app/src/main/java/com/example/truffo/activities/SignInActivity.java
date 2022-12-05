47830ef6ef6eea4100e0a8beee92503034c190a5
BlackHoz666 <fuzzy167it00@gmail.com>
2022-12-05 15:01:28 +0700

    add firebase

--- app/src/main/java/com/example/truffo/activities/SignInActivity.java
+++ app/src/main/java/com/example/truffo/activities/SignInActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.truffo.R;
 import com.example.truffo.databinding.ActivitySignInBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -24,5 +28,24 @@ public class SignInActivity extends AppCompatActivity {
     {
         binding.textCreateNewButton.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
+        binding.buttonSignIn.setOnClickListener(view -> addDataToFirestore());
     }
+
+    private void addDataToFirestore()
+    {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String,Object> data = new HashMap<>();
+        data.put("first_name","Tran");
+        data.put("last_name","Kien");
+        database.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getApplicationContext(),"Data inserted",Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(exception -> {
+                    Toast.makeText(getApplicationContext(),exception.getMessage(),Toast.LENGTH_SHORT).show();
+                });
+    }
+
+
 }
