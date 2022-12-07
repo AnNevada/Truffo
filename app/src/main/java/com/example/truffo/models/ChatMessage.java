0751692d4df73e3a4ad6fb4ed996ec08be45fb80
Nguyen Hoang Nguyen <hoangnguyen789a@gmail.com>
2022-12-07 17:15:24 +0700

    add real-time chat functionality

--- app/src/main/java/com/example/truffo/models/ChatMessage.java
+++ app/src/main/java/com/example/truffo/models/ChatMessage.java
@@ -1,6 +1,8 @@
 package com.example.truffo.models;
 
+import java.util.Date;
+
 public class ChatMessage {
     public String senderID, receiverID, message, dateTime;
-
+    public Date dateObject;
 }
